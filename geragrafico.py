0fa672c422542c5bec97c7f375413ec4d0acf6ab
Nathalia <nathalialopesdelima8@gmail.com>
2024-03-15 09:28:18 -0300

    Função principal do programa em novo arquivo

---
+++ geragrafico.py
@@ -0,0 +1,9 @@
+
+from leitorarquivo import LeitorArquivo
+
+
+if __name__ == '__main__':
+    leitor = LeitorArquivo("data.txt")
+    listaValores = leitor.getValores()
+    print(listaValores)
+       
